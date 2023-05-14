f93314c6bb4b84b9849858a05e6b4fc89d9a1c43
Anway Somani <95833249+aksared@users.noreply.github.com>
2023-05-15 02:40:09 +0530

    Removed invalid declaration

--- zoom/android/src/main/java/com/webcare/zoom/ZoomPlugin.java
+++ zoom/android/src/main/java/com/webcare/zoom/ZoomPlugin.java
@@ -181,7 +181,7 @@ public class ZoomPlugin implements FlutterPlugin, MethodCallHandler,ActivityAwar
         
         StartMeetingParamsWithoutLogin params = new StartMeetingParamsWithoutLogin();
 
-        params.userId = options.get("userId");
+        //params.userId = options.get("userId");
         params.displayName = options.get("displayName");
         params.meetingNo = options.get("meetingId");
         params.userType = MeetingService.USER_TYPE_API_USER;
